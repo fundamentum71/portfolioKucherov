cf51ca74a55a596d7864e0198e7137d2ad726a81
Aleksandr <it.kucherov@gmail.com>
2022-04-27 17:40:36 +0300

    fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -124,6 +124,27 @@ $(document).ready(function () {
 			.eq($(this).index())
 			.addClass('works__items_active');
 	});
+
+	//! mailer
+	$('form').submit(function (e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize(),
+		}).done(function () {
+			$(this).find('input').val('');
+			//$('#consultation, #order').fadeOut();
+			//$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
 
 //!theme-dark
